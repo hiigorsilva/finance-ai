a5d9b0a51d95f004dd4a5eae46ae0380ebbfd461
hiigorsilva <higorscontato@gmail.com>
2024-11-12 16:20:21 -0300

    fix: upsert transaction function

--- src/actions/upsert-transaction/index.ts
+++ src/actions/upsert-transaction/index.ts
@@ -28,11 +28,12 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
   }
 
   await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
+
     where: {
       id: params.id ?? '',
     },
-    update: { ...params, userId },
-    create: { ...params, userId },
   })
   revalidatePath('/transactions')
 }
