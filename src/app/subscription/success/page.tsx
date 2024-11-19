e13456330c17ced11d1709a8cfcfeb9394a66c00
hiigorsilva <higorscontato@gmail.com>
2024-11-19 11:59:13 -0300

    feat: create success page

---
+++ src/app/subscription/success/page.tsx
@@ -0,0 +1,11 @@
+const SuccessPage = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h1 className="font-normal text-5xl text-center">
+        Sua assinatura foi criada com sucesso!
+      </h1>
+    </div>
+  )
+}
+
+export default SuccessPage
