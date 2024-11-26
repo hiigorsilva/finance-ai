661bdade92c4bfe263434a3d61dd755183eb82a6
hiigorsilva <higorscontato@gmail.com>
2024-11-26 17:18:07 -0300

    feat: add visual feedback when the list is empty

--- src/app/(home)/components/expenses-per-category.tsx
+++ src/app/(home)/components/expenses-per-category.tsx
@@ -3,6 +3,7 @@ import { Progress } from '@/components/ui/progress'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { TRANSACTION_CATEGORY_LABELS } from '@/constants/transactions'
 import type { TotalExpensePerCategory } from '@/data/get-dashboard/types'
+import { ScrollIcon } from 'lucide-react'
 
 type ExpensesPerCategoryProps = {
   expensesPerCategory: TotalExpensePerCategory[]
@@ -15,7 +16,9 @@ export const ExpensesPerCategory = ({
     <ScrollArea className="h-full col-span-2 rounded-md border p-0">
       <Card className="h-full bg-transparent border-0">
         <CardHeader>
-          <CardTitle className="font-bold">Gastos por categoria</CardTitle>
+          <CardTitle className="font-bold text-center">
+            Gastos por categoria
+          </CardTitle>
         </CardHeader>
 
         <CardContent className="h-full flex flex-col gap-6">
@@ -34,6 +37,15 @@ export const ExpensesPerCategory = ({
               <Progress className="h-3" value={category.percentageOfTotal} />
             </div>
           ))}
+
+          {expensesPerCategory.length === 0 && (
+            <div className="flex flex-col justify-center items-center gap-2 text-center pt-16">
+              <ScrollIcon strokeWidth={1} size={32} />
+              <span className="text-muted-foreground">
+                Nenhuma categoria registrada.
+              </span>
+            </div>
+          )}
         </CardContent>
       </Card>
     </ScrollArea>
